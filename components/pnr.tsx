a1e769057fa1ae5742e85612a79f1eb93fd74b09
Zishan <zishanmohammed02@gmail.com>
2023-09-03 23:07:45 +0530

    changes

--- components/pnr.tsx
+++ components/pnr.tsx
@@ -17,21 +17,21 @@ const router = useRouter();
     }
    
     return (
-        <div>
-            <div className="container mx-auto bg-sky-500">
+        <div className="m-5 p -10 max-w-screen-xl mx-auto ">
+            <div className="max-w-2xl">
             <form >
-                <div className="space-y-12">
+                <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    
                         <div className="sm:col-span-3">
                             <label htmlFor="train-pnr" className="block text-sm font-medium leading-6 text-gray-900">Pnr No.</label>
                             <div className="mt-2">
-                                <input type="text" value={pnr} onChange={({target})=>setPnr(target?.value)} name="pnr" id="pnr" autoComplete="given-pnr" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <input type="text" value={pnr} onChange={({target})=>setPnr(target?.value)} name="pnr" id="pnr" autoComplete="given-pnr" className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
-                    </div>
+                  
           <div className="mt-2 flex items-center gap-x-3">
-         <button  onClick={searchPNR} type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Search</button>
+         <button  onClick={searchPNR} type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Search</button>
           </div>
         </div> 
         </form></div>
